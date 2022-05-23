71d4b482e77225723d3b7b2f4689f53e5ef2d9f9
ashley068 <35573008+ashley068@users.noreply.github.com>
2022-05-22 22:06:39 -0400

    Update App.js

--- vehicle-inventory-management/src/App.js
+++ vehicle-inventory-management/src/App.js
@@ -5,7 +5,6 @@ import VehicleItem from "./component/VehicleItem";
 import "./App.css";
 
 const mapStateToProps = (state) => ({ vehicleItems: state.vehicle.items });
-//move to item index
 
 const App = ({ vehicleItems }) => {
   return (
